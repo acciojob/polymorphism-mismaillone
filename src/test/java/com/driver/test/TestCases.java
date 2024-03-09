6b2fde7a7a9d79c9e8e2bc36b189a5985955e1ac
Mohammad Ismail <101501734+mismaillone@users.noreply.github.com>
2024-03-09 22:18:02 +0530

    Reset TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,35 +4,5 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-     static class Product {
-        public int product(int x, int y ) {
-            return  x * y;
-        }
-
-        public int product(int x, int y, int z) {
-            return x + y + z ;
-        }
-
-        public double product(double x, double y)
-        {
-            return  x / y;
-        }
-    }
-
-
-
-    public static void main(String[] args) {
-            Product p = new Product();
-
-            int result = p.product(5, 4);
-            System.out.println(result);
-            int sum = p.product(12, 5, 3);
-
-            System.out.println(sum);
-
-            double div  = p.product(12.5, 1.5);
-
-            System.out.println(div);
-
-    }
+    
 }
