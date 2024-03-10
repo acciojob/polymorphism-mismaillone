d3dbd5d94b9a069952ed1a07e84d0d9690c70cea
mismaillone <warlockmachine34@gmail.com>
2024-03-10 04:54:07 -0400

    Assignment Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+		 static class Product {
 
+        public int product(int x, int y) {
+            return x + y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y + z;
+        }
+
+        public double product(double x, double y) {
+            return x - y;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        double res = p.product(15.5, 15.2); 
+        int sum = p.product(12, 5);
+        int sub = p.product(15, 12);
+        System.out.println(sum);
+        System.out.println(res);
+        System.out.println(sub);
+    }
 }
